98a7fcd7ffaf36437ebebeb9ace2f047461ab776
William Barbosa <william.sb@hotmail.com>
2018-08-09 16:36:51 -0300

    :construction: Introduce the BaseCalendarService

--- Toggl.Daneel/Services/CalendarService.cs
+++ Toggl.Daneel/Services/CalendarService.cs
@@ -5,48 +5,24 @@ using System.Reactive.Linq;
 using EventKit;
 using Toggl.Foundation.Calendar;
 using Toggl.Foundation.MvvmCross.Services;
-using Toggl.Foundation.Services;
 using Toggl.Multivac;
 using Toggl.PrimeRadiant.Settings;
 
 namespace Toggl.Daneel.Services
 {
-    public sealed class CalendarService : ICalendarService
+    public sealed class CalendarService : BaseCalendarService
     {
-        private readonly IUserPreferences userPreferences;
-        private readonly IPermissionsService permissionsService;
         private readonly EKEventStore eventStore = new EKEventStore();
 
-        public CalendarService(
-            IUserPreferences userPreferences,
-            IPermissionsService permissionsService)
+        public CalendarService(IPermissionsService permissionsService)
+            : base (permissionsService)
         {
-            Ensure.Argument.IsNotNull(userPreferences, nameof(userPreferences));
-            Ensure.Argument.IsNotNull(permissionsService, nameof(permissionsService));
-
-            this.userPreferences = userPreferences;
-            this.permissionsService = permissionsService;
         }
 
-        public IObservable<IEnumerable<CalendarItem>> GetEventsForDate(DateTime date)
+        public override IObservable<IEnumerable<CalendarItem>> GetEventsForDate(DateTime date)
             => Observable.Return(new List<CalendarItem>());
 
-        public IObservable<IEnumerable<UserCalendar>> UserCalendars
-            => Observable.DeferAsync(async (cancellactionToken) =>
-                {
-                    var isAuthorized = await permissionsService.CalendarAuthorizationStatus;
-                    if (!isAuthorized)
-                    {
-                        return Observable.Throw<IEnumerable<UserCalendar>>(
-                            new NotAuthorizedException("You don't have permission to access calendars")
-                        );
-                    }
-
-                    var enabledIds = userPreferences.EnabledCalendarIds().ToHashSet();
-                    return Observable.Return(selectCalendars(enabledIds));
-                });
-
-        private IEnumerable<UserCalendar> selectCalendars(HashSet<string> selectedIds)
+        protected override IEnumerable<UserCalendar> NativeGetUserCalendars()
             => eventStore
                 .GetCalendars(EKEntityType.Event)
                 .Select(ekCalendar => userCalendarFromEKCalendar(
